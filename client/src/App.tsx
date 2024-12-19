37c70175b5af89d3b1073a7064fa94b488c1c748
RhamzHam <RhamzHam>
2024-12-19 20:35:47 +0800

    fix: set theme to dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import {useState, useEffect} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -11,40 +11,41 @@ function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/fetch_subtasks')
-    .then((res) => {
-        setData(res.data)
-        console.log("FETCHED")
+    axios
+      .get("http://localhost:3000/fetch_subtasks")
+      .then((res) => {
+        setData(res.data);
+        console.log("FETCHED");
         //console.log(data)
-    })
-    .catch((err) => console.log(err))
-  }, [refresh])
+      })
+      .catch((err) => console.log(err));
+  }, [refresh]);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element = {
-          <div className="bg-dark">
-            <Navbar />
-            <div className="container-fluid">
-              <div className="row flex-nowrap">
-                <div className="col-auto shadow">
-                  <Sidebar
-                    refresh = {refresh}
-                    setRefresh = {setRefresh}
-                  />
-                </div>
-                <div className="col py-3">
-                  <Table 
-                    data = {data} 
-                    refresh = {refresh} 
-                    setRefresh = {setRefresh}
-                  />
+        <Route
+          path="/"
+          element={
+            <div className="bg-dark" data-bs-theme="dark">
+              <Navbar />
+              <div className="container-fluid">
+                <div className="row flex-nowrap">
+                  <div className="col-auto shadow">
+                    <Sidebar refresh={refresh} setRefresh={setRefresh} />
+                  </div>
+                  <div className="col py-3">
+                    <Table
+                      data={data}
+                      refresh={refresh}
+                      setRefresh={setRefresh}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        } />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
