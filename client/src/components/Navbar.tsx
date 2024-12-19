97087ca1b2b4a2582adb6e348741f4f4f4c8fc85
RhamzHam <RhamzHam>
2024-12-19 19:50:05 +0800

    feat: sidebar styling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
-    <nav className="navbar sticky-top bg-dark-subtle container-fluid justify-content-center shadow-lg">
-      <header className="d-flex justify-content-center">
+    <nav className="navbar sticky-top bg-dark-subtle container-fluid justify-content-center">
+      <header className="d-flex justify-content-center py-2">
         <img src={logo} width={340} height={63} />
       </header>
     </nav>
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,12 +5,16 @@ import Create from "../crud_components/create";
 const Sidebar = () => {
   const [add_subtask_popup, set_popup] = useState(false);
   return (
-    <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-      <h1>Sidebar</h1>
-      <hr />
+    <div className="d-flex flex-column px-3 pt-2 text-white min-vh-100">
+      <h2 className="py-2">Tools</h2>
+      <hr className="my-0" />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item p-3">Search</li>
-        <button id="add_subtask_btn" onClick={() => set_popup(true)}>
+        <li className="nav-item py-3">Search</li>
+        <button
+          className="btn btn-secondary"
+          id="add_subtask_btn"
+          onClick={() => set_popup(true)}
+        >
           ADD SUBTASK
         </button>
         <Create trigger={add_subtask_popup} setTrigger={set_popup} />
